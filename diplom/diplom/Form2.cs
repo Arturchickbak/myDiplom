6065690b8d65951c88d74b82f4ca5128618d2f5a
Arturchickbak <83332216+Arturchickbak@users.noreply.github.com>
2024-03-19 17:15:54 +0300

    Add files via upload

---
+++ diplom/diplom/Form2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace diplom
+    
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+
+        {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+
+        {
+            int startReading = Convert.ToInt32(textBox1.Text);
+            int endReading = Convert.ToInt32(textBox2.Text);
+
+            // Вычисление разницы между начальными и конечными показаниями счетчика
+            int consumption = endReading - startReading;
+
+            // Тарифная ставка за единицу потребления (например, 1 кВт⋅ч)
+            double tariffRate = 4.81; // Пример тарифа
+
+            // Расчет суммы к оплате
+            double totalPayment = consumption * tariffRate;
+
+            // Вывод результата пользователю
+            MessageBox.Show($"К оплате: {totalPayment} рублей");
+            
+        }
+        
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            
+        }
+        
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
---
+++ diplom/diplom/Form4.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace diplom
+{
+    partial class Form4
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(-50, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(821, 63);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(257, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(305, 42);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Личный кабинет";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Location = new System.Drawing.Point(-32, 70);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(785, 235);
+            this.panel3.TabIndex = 3;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.White;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button4.Location = new System.Drawing.Point(552, 108);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(114, 48);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Свет";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(252, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Выберите сферу:";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.White;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button3.Location = new System.Drawing.Point(332, 108);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(114, 48);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Газ";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.Location = new System.Drawing.Point(97, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 48);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Вода";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(721, 300);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form4";
+            this.Text = "Kabinet";
+            this.Load += new System.EventHandler(this.Kabinet_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button4;
+    }
+}
